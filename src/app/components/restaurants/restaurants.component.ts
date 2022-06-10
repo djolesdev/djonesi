8c72172a0dcf1b5b836f4ea34ec449b8c3ec8b2f
Djordje Stefanovic <djolesdev@gmail.com>
2022-06-10 15:03:37 +0200

    added models and assets

--- src/app/components/restaurants/restaurants.component.ts
+++ src/app/components/restaurants/restaurants.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-restaurants',
   templateUrl: './restaurants.component.html',
   styleUrls: ['./restaurants.component.css']
 })
 export class RestaurantsComponent implements OnInit {
+
+
+  link: string = '../../../assets/420.jpeg'
 
   constructor() { }
 
---
+++ src/app/models/Restaurant.ts
@@ -0,0 +1,5 @@
+export interface Restaurant {
+    name: string,
+    type: string,
+    imgUrl: string
+}
