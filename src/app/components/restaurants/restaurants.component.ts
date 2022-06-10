ee3e5a3f36b58c492d6bcf546f603b2e2603ef2b
Djordje Stefanovic <djolesdev@gmail.com>
2022-06-10 16:18:29 +0200

    added restaurant module

--- src/app/components/restaurants/restaurants.component.ts
+++ src/app/components/restaurants/restaurants.component.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Restaurant } from 'src/app/models/Restaurant';
+import { RestaurantService } from 'src/app/services/restaurant/restaurant.service';
 
 @Component({
   selector: 'app-restaurants',
   templateUrl: './restaurants.component.html',
-  styleUrls: ['./restaurants.component.css']
+  styleUrls: ['./restaurants.component.css'],
 })
 export class RestaurantsComponent implements OnInit {
+  restaurants: Restaurant[] = [];
 
-
-  link: string = '../../../assets/420.jpeg'
-
-  constructor() { }
+  constructor(private service: RestaurantService) {}
 
   ngOnInit(): void {
+    this.service.getAllRestaurants().subscribe((responce) => {
+      responce.forEach((el) => {
+        const data: any = el.payload.doc.data();
+        this.restaurants.push({
+          name: data.name,
+          imgUrl: data.imgUrl,
+          type: data.type,
+        });
+      });
+    });
   }
 
+  djole() {
+    console.log(this.restaurants)
+  }
 }
--- src/app/services/restaurant/restaurant.service.ts
+++ src/app/services/restaurant/restaurant.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore'
+import { Observable } from 'rxjs';
+import { Restaurant } from 'src/app/models/Restaurant';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestaurantService {
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore) { }
+
+  getAllRestaurants() {
+    return this.firestore.collection('/restaurants').snapshotChanges()
+  }
 }
